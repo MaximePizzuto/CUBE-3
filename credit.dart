00eae4df010bf53bcb5bda9871af6acfec1c486b
dorian DI <70703230+DorianDI@users.noreply.github.com>
2023-07-18 09:33:40 +0200

    Update credit.dart

--- credit.dart
+++ credit.dart
@@ -11,7 +11,7 @@ class Credit extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.red,
       ),
-      home: const CreditScreen(title: 'MyPerform'),
+      home: const CreditScreen(title: 'Crédit'),
     );
   }
 }
@@ -33,7 +33,20 @@ class CreditScreenState extends State<CreditScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: const Color.fromRGBO(230, 93, 43, 1.0),
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color(0xFFBF360C),
+                Color(0xFFF57C00),
+                Color(0xFFBF360C),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              transform: GradientRotation(45),
+            ),
+          ),
+        ),
         title: Text(
           widget.title,
           style: const TextStyle(
@@ -62,7 +75,7 @@ class CreditScreenState extends State<CreditScreen> {
                 child: Align(
                   alignment: Alignment.bottomRight,
                   child: SizedBox(
-                    width: 90,
+                    width: 85,
                     height: 80,
                     child: Image.asset(
                       'assets/images/myperform.png',
@@ -72,137 +85,168 @@ class CreditScreenState extends State<CreditScreen> {
                 ),
               ),
             ),
+            const Positioned(
+              bottom: 1,
+              left: 0,
+              right: 0,
+              child: Center(
+                child: Text(
+                  '©2023 MyPerform | Developed By Dorian',
+                  style: TextStyle(
+                    fontFamily: 'merriweather',
+                    fontSize: 8,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ),
             const SizedBox(height: 100),
             Align(
               alignment: Alignment.center,
-              child: Column(
-                children: [
-                  Align(
-                    child: Container(
-                      width: 250,
-                      height: 110,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10.0),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black.withOpacity(0.2),
-                            blurRadius: 6.0,
-                            spreadRadius: 2.0,
-                            offset: const Offset(0, 3),
+              child: Container(
+                width: 300,
+                height: 150,
+                padding: const EdgeInsets.all(20.0),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10.0),
+                  boxShadow: const [
+                    BoxShadow(
+                      offset: Offset(3, 3),
+                      spreadRadius: -3,
+                      blurRadius: 5,
+                      color: Color.fromRGBO(246, 235, 20, 0.5),
+                    )
+                  ],
+                ),
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Align(
+                        child: Container(
+                          width: 250,
+                          height: 110,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10.0),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black.withOpacity(0.2),
+                                blurRadius: 6.0,
+                                spreadRadius: 2.0,
+                                offset: const Offset(0, 3),
+                              ),
+                            ],
                           ),
-                        ],
-                      ),
-                      child: Stack(
-                        children: [
-                          Container(
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10.0),
-                            ),
-                          ),
-                          Container(
-                            padding: const EdgeInsets.all(20.0),
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.circular(10.0),
-                            ),
-                            child: const Column(
-                              children: [
-                                Text(
-                                  'Nos développeurs',
-                                  style: TextStyle(
-                                    fontFamily: 'merriweather',
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.black,
-                                  ),
+                          child: Stack(
+                            children: [
+                              Container(
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10.0),
                                 ),
-                                SizedBox(height: 5.0),
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
+                              ),
+                              Container(
+                                padding: const EdgeInsets.all(20.0),
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(10.0),
+                                ),
+                                child: const Column(
                                   children: [
                                     Text(
-                                      'Maxime',
+                                      'Nos développeurs',
                                       style: TextStyle(
                                         fontFamily: 'merriweather',
-                                        fontSize: 15,
+                                        fontSize: 20,
                                         fontWeight: FontWeight.bold,
                                         color: Colors.black,
                                       ),
                                     ),
-                                    SizedBox(width: 10.0),
-                                    Text(
-                                      'Quentin',
-                                      style: TextStyle(
-                                        fontFamily: 'merriweather',
-                                        fontSize: 15,
-                                        fontWeight: FontWeight.bold,
-                                        color: Colors.black,
-                                      ),
+                                    SizedBox(height: 5.0),
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        Text(
+                                          'Maxime',
+                                          style: TextStyle(
+                                            fontFamily: 'merriweather',
+                                            fontSize: 15,
+                                            fontWeight: FontWeight.bold,
+                                            color: Colors.black,
+                                          ),
+                                        ),
+                                        SizedBox(width: 10.0),
+                                        Text(
+                                          'Quentin',
+                                          style: TextStyle(
+                                            fontFamily: 'merriweather',
+                                            fontSize: 15,
+                                            fontWeight: FontWeight.bold,
+                                            color: Colors.black,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    Center(
+                                        child: Text(
+                                          'Dorian',
+                                          style: TextStyle(
+                                            fontFamily: 'merriweather',
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.bold,
+                                            color: Colors.black,
+                                          ),
+                                        ),
                                     ),
                                   ],
                                 ),
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    Text(
-                                      'Dorian',
-                                      style: TextStyle(
-                                        fontFamily: 'merriweather',
-                                        fontSize: 15,
-                                        fontWeight: FontWeight.bold,
-                                        color: Colors.black,
-                                      ),
-                                    ),
-                                    SizedBox(width: 10.0),
-                                    Text(
-                                      'Théo',
-                                      style: TextStyle(
-                                        fontFamily: 'merriweather',
-                                        fontSize: 15,
-                                        fontWeight: FontWeight.bold,
-                                        color: Colors.black,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ],
-                            ),
+                              ),
+                            ],
                           ),
-                        ],
+                        ),
                       ),
-                    ),
+                    ],
                   ),
-                ],
+                ),
               ),
             ),
-            const SizedBox(),
             Positioned(
-              bottom: 60,
+              bottom: 180,
               left: 0,
               right: 0,
               child: Container(
                 alignment: Alignment.bottomCenter,
-                child: TextButton(
-                  onPressed: () {
-                    Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(builder: (context) => Connect()),
-                    );
-                  },
-                  style: ButtonStyle(
-                    textStyle: MaterialStateProperty.all<TextStyle>(
-                      const TextStyle(
-                        color: Colors.black,
-                        fontSize: 16.0,
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.0),
+                    gradient: const LinearGradient(
+                      colors: [
+                        Color(0xFFBF360C),
+                        Color(0xFFF57C00),
+                        Color(0xFFBF360C),
+                      ],
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                      transform: GradientRotation(45),
+                    ),
+                  ),
+                  child: TextButton(
+                    onPressed: () {
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(builder: (context) => Connect()),
+                      );
+                    },
+                    child: const Text(
+                      'Retour',
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 0, 0, 0),
                         fontWeight: FontWeight.bold,
                       ),
                     ),
-                    backgroundColor: MaterialStateProperty.all<Color>(
-                        const Color.fromARGB(255, 193, 91, 1)),
-                    foregroundColor:
-                        MaterialStateProperty.all<Color>(Colors.black),
                   ),
-                  child: const Text('Retour'),
                 ),
               ),
             ),
