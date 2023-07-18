4a9356e3f30ea0047b5ae319419dbddae35a9fae
QuentinAlv <quentin.alvernhe@gmail.com>
2023-07-18 15:24:50 +0200

    Premiere APP VueJS

---
+++ MyPerformAbonnement/src/components/HomeComponents.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="homeContainer">
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "Bonjour le monde",
+    }
+  }
+}
+</script>
+
+<style src="../assets/home.css" scoped>
+
+</style>
